c35a5b858789809ac1b4bf47e4e5ea465fe7d81b
Munna-Kumar-1432 <munnakumar6204835946@gmail.com>
2025-04-24 00:47:13 +0530

    change

--- components/models/invite-model.tsx
+++ components/models/invite-model.tsx
@@ -1,8 +1,5 @@
 "use client";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import axios from "axios";
+
 import {
   Dialog,
   DialogContent,
@@ -15,12 +12,44 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { Copy, RefreshCw } from "lucide-react";
+import { Check, Copy, RefreshCw } from "lucide-react";
+import { useOrigin } from "@/hooks/use-origin";
+import { useState } from "react";
+import axios from "axios";
 
 export const InviteModal = () => {
-  const { isOpen, onClose, type } = useModal();
+  const { onOpen,isOpen, onClose, type, data } = useModal();
 
+  const { server } = data;
   const isModalOpen = isOpen && type === "invite";
+
+  const origin = useOrigin();
+
+  const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
+  const [copied, setCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(inviteUrl);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  const onNew = async()=>{
+    try {
+      setIsLoading(true)
+      const response = await axios.patch(`/api/servers/${server?.id}/invite-code`)
+
+      onOpen("invite",{server:response.data})
+    } catch (error) {
+     console.log(error) 
+    } finally{
+      setIsLoading(false)
+    }
+  }
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -36,14 +65,18 @@ export const InviteModal = () => {
           </Label>
           <div className="flex items-center mt-2 gap-x-2">
             <Input
+            disabled={isLoading}
               className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-              value="Invite Links"
+              value={inviteUrl}
             />
-            <Button size={"icon"}>
-              <Copy className="size-4" />
+            <Button disabled={isLoading} onClick={onCopy} size={"icon"}>
+              {copied ? <Check className="size-4"/> :  <Copy className="size-4" />}
+             
             </Button>
           </div>
           <Button
+          onClick={onNew}
+          disabled={isLoading}
             variant={"link"}
             size={"sm"}
             className="text-xs text-zinc-500 mt-4"
